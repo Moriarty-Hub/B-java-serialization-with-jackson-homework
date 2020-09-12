b1e31f51c4cea90ccf758363106ca5fc77030cc9
Xiangrui Dong <samdongxiangrui@outlook.com>
2020-09-12 14:41:18 +0800

    [xdong] finish problem 2

--- src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
@@ -1,16 +1,30 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.io.IOException;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class PersonVo {
 
+  private static final Integer DEFAULT_AGE_VALUE = 0;
+
   private String id;
   private Integer age;
   private String name;
+  @JsonInclude(JsonInclude.Include.NON_NULL)
   private String hobby;
+
+  public Integer getAge() {
+    return age ==null ? DEFAULT_AGE_VALUE : age;
+  }
 }
